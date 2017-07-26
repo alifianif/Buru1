70e59c48907d795b31f6ce13d38439f057cdb7cd
ramantidwi <ramantidwi82@gmail.com>
2017-07-26 14:33:58 +0700

    Add project files.

---
+++ Buru/Controllers/BugController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Buru.Models;
+using System.IO;
+
+namespace Buru.Controllers
+{
+    public class BugController : Controller
+    {
+        private BuruDataEntities2 db = new BuruDataEntities2();
+
+        // GET: Bug
+        public ActionResult Index()
+        {
+
+            return View(db.Bugs.ToList());
+
+        }
+
+        // GET: Bug/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bug bug = db.Bugs.Find(id);
+            if (bug == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bug);
+        }
+
+        // GET: Bug/Create
+        public ActionResult Create()
+        {
+            List<string> BugKind = new List<string>();
+            List<string> BugSeverity = new List<string>();
+
+            BugKind.Add("Bug");
+            BugKind.Add("Feedback");
+
+            BugSeverity.Add("Critical");
+            BugSeverity.Add("High");
+            BugSeverity.Add("Medium");
+            BugSeverity.Add("Low");
+
+            ViewBag.BugKind = BugKind;
+            ViewBag.BugSeverity = BugSeverity;
+            return View();
+        }
+
+        // POST: Bug/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BugId,Title,Description,Steps,Attachment,Assignee,CreatedDate,ClosedDate,Severity,Kind,Status")] Bug bug)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bugs.Add(bug);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bug);
+        }
+
+        // GET: Bug/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bug bug = db.Bugs.Find(id);
+            if (bug == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bug);
+        }
+
+        // POST: Bug/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BugId,Title,Description,Steps,Attachment,Assignee,CreatedDate,ClosedDate,Severity,Kind,Status")] Bug bug)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bug).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bug);
+        }
+
+        // GET: Bug/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bug bug = db.Bugs.Find(id);
+            if (bug == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bug);
+        }
+
+        // POST: Bug/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bug bug = db.Bugs.Find(id);
+            db.Bugs.Remove(bug);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+       
+
+
+
+    } }
---
+++ Buru/Models/UploadFile.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.XPath;
+
+namespace Buru.Models
+{
+    public class UploadFile
+    {
+       /* [Required, Microsoft.Web.Mvc.FileExtensions(Extensions = "csv",
+             ErrorMessage = "Specify a CSV file. (Comma-separated values)")]
+        public HttpPostedFileBase File { get; set; }*/
+    }
+}
